c70d691c054579036faf6689f60df82a5bf1d34a
pawsplay <dillon.w.gu@gmail.com>
2015-02-11 15:32:15 -0600

    Added GraveyardTest scene

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class PlayerAttack : MonoBehaviour {
 	public GameObject target;
 	public float attackTimer;
-	public float smashTimer;
 	public float cooldown;
 	public float smashCooldown;
 
@@ -12,39 +11,35 @@ public class PlayerAttack : MonoBehaviour {
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
-	public float attackSpeed = 1.3F;
+	public float attackSpeed = 3.0F;
+
 	public AnimationClip attackAnimation1;
 	public AnimationClip attackAnimation2;
 	public AnimationClip attackAnimation3;
 	public AnimationClip smashAnimation1;
 	public AnimationClip smashAnimation2;
 	public AnimationClip smashAnimation3;
-	public AnimationClip smashAnimation4;
 	public AnimationClip returnAnimation1;
 	public AnimationClip returnAnimation2;
 	public AnimationClip returnAnimation3;
 	public AnimationClip returnSmashAnimation1;
 	public AnimationClip returnSmashAnimation2;
 	public AnimationClip returnSmashAnimation3;
-	public AnimationClip returnSmashAnimation4;
-
 	public AnimationClip defendAnimation;
 	public AnimationClip returnDefendAnimation;
 
 	private bool attacking = false;
-	private int attackSequence = 0;
 	private bool smashing = false;
+	private bool defending = false;
+	private int attackSequence = 0;
 	private bool willAttack = false;
 	private bool willSmash = false;
-
-	private bool defending = false;
 
 	// Use this for initialization
 	void Start () {
 		attackTimer = 0;
-		attackTimer = 0;
 		cooldown = animation[attackAnimation1.name].length - 0.8f;
-		smashCooldown = animation[smashAnimation1.name].length + 0.8f;
+		smashCooldown = animation[attackAnimation1.name].length + 0.8f;
 	}
 	
 	// Update is called once per frame
@@ -54,18 +49,12 @@ public class PlayerAttack : MonoBehaviour {
 		if (attackTimer < 0)
 			attackTimer = 0;
 		if (Input.GetMouseButtonDown(0)){
+			Debug.Log ("attacking = "+attacking);
 			if (attackTimer == 0){
 				if (attacking){
-					/*if (attackSequence == 0){
-						attackSequence++;
-						Attack();
-						attackTimer = cooldown;
-					}*/
-					//else
+					if (attackSequence < 2){
 						willAttack = true;
-				}
-				else if (defending){
-					return;
+					}
 				}
 				else{
 					Attack();
@@ -74,11 +63,13 @@ public class PlayerAttack : MonoBehaviour {
 			}
 		}
 		if (Input.GetMouseButtonDown(1)){
-			if (attackTimer == 0){
-				if (attacking){
+			if (attacking){
+				Debug.Log ("isplaying?");
+				if (attackTimer == 0)
 					willSmash = true;
-				}
-				else{
+			}
+			else{
+				if (attackTimer == 0){
 					if (!defending){
 						Defend();
 						//attackTimer = smashCooldown;
@@ -86,37 +77,53 @@ public class PlayerAttack : MonoBehaviour {
 				}
 			}
 		}
-		else if (Input.GetMouseButtonUp(1)){
+		if (Input.GetMouseButtonUp(1)){
 			if (defending){
 				ReleaseDef();
 			}
 		}
 		if (smashing){
+			Debug.Log ("isplaying?");
 			if (!animation.isPlaying){
 				/*if (attackSequence == 0){
 					animation.Play (returnSmashAnimation1.name);
 				}*/
 				if (attackSequence == 0){
-					animation.Play (returnSmashAnimation2.name);
+					animation.Play (returnSmashAnimation1.name);
 				}
 				else if (attackSequence == 1){
-					animation.Play (returnSmashAnimation3.name);
+					animation.Play (returnSmashAnimation2.name);
 				}
 				else if (attackSequence == 2){
-					animation.Play (returnSmashAnimation4.name);
+					animation.Play (returnSmashAnimation3.name);
+				}
+				if (!animation.isPlaying){
+					if (attackSequence == 0){
+						animation.Play (returnAnimation1.name);
+					}
+					else if (attackSequence == 1){
+						animation.Play (returnAnimation2.name);
+					}
+					else if (attackSequence == 2){
+						animation.Play (returnAnimation3.name);
+					}
+					attacking = false;
+					attackSequence = 0;
+					Debug.Log ("return animation. attacking = false and sequence reset");
 				}
 				smashing = false;
 				attacking = false;
 				attackSequence = 0;
 				//Debug.Log ("return animation. attacking = false and sequence reset");
 			}
-			else {
-				//Debug.Log("Doing nothing bro");
-			}
 		}
-		else if (attacking){
+
+		if (attacking){
+			Debug.Log ("isplaying?");
 			if (attackTimer == 0){
+					Debug.Log ("isplaying?");
 				if (!animation.isPlaying){
+					Debug.Log ("return animation. attacking = false and sequence reset: "+attackSequence);
 					if (willSmash){
 						willAttack = false;
 						SmashAttack ();
@@ -151,7 +158,7 @@ public class PlayerAttack : MonoBehaviour {
 		float distance = Vector3.Distance(target.transform.position, transform.position);
 		Vector3 dir = (target.transform.position - transform.position).normalized;
 		float direction = Vector3.Dot (dir, transform.forward);
-
+		Debug.Log("boo");
 		attacking = true;
 		willAttack = false;
 		if (attackSequence == 0){
@@ -173,7 +180,7 @@ public class PlayerAttack : MonoBehaviour {
 		else if (attackSequence == 2){
 			animation[attackAnimation3.name].speed = attackSpeed;
 			animation.Play(attackAnimation3.name);
-			if (distance < 3f){
+			if (distance < 4f){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-15);
 			}
@@ -184,35 +191,33 @@ public class PlayerAttack : MonoBehaviour {
 		Vector3 dir = (target.transform.position - transform.position).normalized;
 		float direction = Vector3.Dot (dir, transform.forward);
 		
-		smashing = true;
 		attacking = true;
+		smashing = true;
 		willSmash = false;
-		willAttack = false;
 		if (attackSequence == 0){
-			animation[smashAnimation2.name].speed = attackSpeed;
-			animation.Play(smashAnimation2.name);
+			animation[smashAnimation1.name].speed = attackSpeed;
+			animation.Play(smashAnimation1.name);
 			if (distance < 3f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
-				eh.AdjustCurrentHealth(-15);
+				eh.AdjustCurrentHealth(-10);
 			}
 		}
 		else if (attackSequence == 1){
-			animation[smashAnimation3.name].speed = attackSpeed;
-			animation.Play(smashAnimation3.name);
-			if (distance < 3.5f && direction > 0){
+			animation[smashAnimation2.name].speed = attackSpeed;
+			animation.Play(smashAnimation2.name);
+			if (distance < 4f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-20);
 			}
 		}
 		else if (attackSequence == 2){
-			animation[smashAnimation4.name].speed = attackSpeed;
-			animation.Play(smashAnimation4.name);
-			if (distance < 3.5f && direction > 0){
+			animation[smashAnimation3.name].speed = attackSpeed;
+			animation.Play(smashAnimation3.name);
+			if (distance < 5f){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
-				eh.AdjustCurrentHealth(-35);
+				eh.AdjustCurrentHealth(-15);
 			}
 		}
-		Debug.Log ("yo");
 	}
 	public void Defend () {
 		defending = true;
