99f391bf538c86aefea2872b9722fcd3a77ef011
pawsplay <dillon.w.gu@gmail.com>
2015-02-12 03:14:01 -0600

    Attack Animation working with movement
    Only if TP_Animator is unchecked

--- Assets/Scripts/ThirdPersonControl/TP_Controller.cs
+++ Assets/Scripts/ThirdPersonControl/TP_Controller.cs
@@ -104,8 +104,6 @@ public class TP_Controller : MonoBehaviour
 		if (attackTimer < 0)
 			attackTimer = 0;
 		if (Input.GetMouseButtonDown(0)) {
-			//TP_Animator.Instance.Attacking();
-			Debug.Log ("attacking = "+attacking);
 			if (attackTimer == 0){
 				if (attacking){
 					if (attackSequence < 2){
@@ -122,7 +120,6 @@ public class TP_Controller : MonoBehaviour
 			//Instantiate (attackParticleSystem, transform.position, transform.rotation);
 			
 			if (attacking){
-				Debug.Log ("isplaying?");
 				if (attackTimer == 0)
 					willSmash = true;
 			}
@@ -151,23 +148,23 @@ public class TP_Controller : MonoBehaviour
 					animation.Play (returnSmashAnimation1.name);
 				}*/
 				if (attackSequence == 0){
-					animation.Play (returnSmashAnimation1.name);
+					animation.CrossFade (returnSmashAnimation1.name);
 				}
 				else if (attackSequence == 1){
-					animation.Play (returnSmashAnimation2.name);
+					animation.CrossFade (returnSmashAnimation2.name);
 				}
 				else if (attackSequence == 2){
-					animation.Play (returnSmashAnimation3.name);
+					animation.CrossFade (returnSmashAnimation3.name);
 				}
 				if (!animation.isPlaying){
 					if (attackSequence == 0){
-						animation.Play (returnAnimation1.name);
+						animation.CrossFade (returnAnimation1.name);
 					}
 					else if (attackSequence == 1){
-						animation.Play (returnAnimation2.name);
+						animation.CrossFade (returnAnimation2.name);
 					}
 					else if (attackSequence == 2){
-						animation.Play (returnAnimation3.name);
+						animation.CrossFade (returnAnimation3.name);
 					}
 					attacking = false;
 					attackSequence = 0;
@@ -183,7 +180,6 @@ public class TP_Controller : MonoBehaviour
 		if (attacking){
 			Debug.Log ("isplaying?");
 			if (attackTimer == 0){
-				Debug.Log ("isplaying?");
 				if (!animation.isPlaying){
 					Debug.Log ("return animation. attacking = false and sequence reset: "+attackSequence);
 					if (willSmash){
@@ -199,13 +195,13 @@ public class TP_Controller : MonoBehaviour
 					else {
 						Debug.Log ("return animation. attacking = false and sequence reset: "+attackSequence);
 						if (attackSequence == 0){
-							animation.Play (returnAnimation1.name);
+							animation.CrossFade (returnAnimation1.name);
 						}
 						else if (attackSequence == 1){
-							animation.Play (returnAnimation2.name);
+							animation.CrossFade (returnAnimation2.name);
 						}
 						else if (attackSequence == 2){
-							animation.Play (returnAnimation3.name);
+							animation.CrossFade (returnAnimation3.name);
 						}
 						attacking = false;
 						attackSequence = 0;
@@ -243,7 +239,7 @@ public class TP_Controller : MonoBehaviour
 		willAttack = false;
 		if (attackSequence == 0){
 			animation[attackAnimation1.name].speed = attackSpeed;
-			animation.Play(attackAnimation1.name);
+			animation.CrossFade(attackAnimation1.name);
 			
 			if (distance < 2.5f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
@@ -252,7 +248,7 @@ public class TP_Controller : MonoBehaviour
 		}
 		else if (attackSequence == 1){
 			animation[attackAnimation2.name].speed = attackSpeed;
-			animation.Play(attackAnimation2.name);
+			animation.CrossFade(attackAnimation2.name);
 			if (distance < 3f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-10);
@@ -260,7 +256,7 @@ public class TP_Controller : MonoBehaviour
 		}
 		else if (attackSequence == 2){
 			animation[attackAnimation3.name].speed = attackSpeed;
-			animation.Play(attackAnimation3.name);
+			animation.CrossFade(attackAnimation3.name);
 			if (distance < 4f){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-15);
@@ -278,7 +274,7 @@ public class TP_Controller : MonoBehaviour
 		willSmash = false;
 		if (attackSequence == 0){
 			animation[smashAnimation1.name].speed = attackSpeed;
-			animation.Play(smashAnimation1.name);
+			animation.CrossFade(smashAnimation1.name);
 			if (distance < 3f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-10);
@@ -286,7 +282,7 @@ public class TP_Controller : MonoBehaviour
 		}
 		else if (attackSequence == 1){
 			animation[smashAnimation2.name].speed = attackSpeed;
-			animation.Play(smashAnimation2.name);
+			animation.CrossFade(smashAnimation2.name);
 			if (distance < 4f && direction > 0){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
 				eh.AdjustCurrentHealth(-20);
@@ -294,22 +290,28 @@ public class TP_Controller : MonoBehaviour
 		}
 		else if (attackSequence == 2){
 			animation[smashAnimation3.name].speed = attackSpeed;
-			animation.Play(smashAnimation3.name);
+			animation.CrossFade(smashAnimation3.name);
+			float animRatio = animation[smashAnimation3.name].time/animation[smashAnimation3.name].length;
+			Debug.Log ("ratio = "+animRatio);
+			if (animRatio == 0.66f){
+				Debug.Log ("ratio");
+				Instantiate (attackParticleSystem, transform.position + transform.up + transform.forward, transform.rotation);
+			}
 			if (distance < 5f){
 				EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
-				eh.AdjustCurrentHealth(-15);
+				eh.AdjustCurrentHealth(-30);
 			}
 		}
 	}
 	public void Defend () {
 		defending = true;
 		//animation[defendAnimation.name].speed = attackSpeed;
-		animation.Play(defendAnimation.name);
+		animation.CrossFade(defendAnimation.name);
 	}
 	public void ReleaseDef() {
 		defending = false;
 		//animation[returnDefendAnimation.name].speed = attackSpeed;
-		animation.Play(returnDefendAnimation.name);
+		animation.CrossFade(returnDefendAnimation.name);
 	}
 
 	//void Attack(int attackNumber)
