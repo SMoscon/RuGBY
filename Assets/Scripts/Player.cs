1ba395afc4c93798b743ddea912e1b2e42bfa04d
baibye <carlbai@gmail.com>
2015-02-13 19:08:03 -0600

    Added smoothness

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public float speed = 10f;
- 
+     
     void Update()
     {
         if (networkView.isMine)
         {
             InputMovement();
         }
+        else
+        {
+            SyncedMovement();
+        }
+    }
+     
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+        rigidbody.position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
     }
  
     void InputMovement()
@@ -27,18 +37,35 @@ public class Player : MonoBehaviour {
         if (Input.GetKey(KeyCode.A))
             rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
     }
-    void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
+     
+void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+{
+    Vector3 syncPosition = Vector3.zero;
+    Vector3 syncVelocity = Vector3.zero;
+    if (stream.isWriting)
     {
-        Vector3 syncPosition = Vector3.zero;
-        if (stream.isWriting)
-        {
-            syncPosition = rigidbody.position;
-            stream.Serialize(ref syncPosition);
-        }
-        else
-        {
-            stream.Serialize(ref syncPosition);
-            rigidbody.position = syncPosition;
-        }
+        syncPosition = rigidbody.position;
+        stream.Serialize(ref syncPosition);
+ 
+        syncVelocity = rigidbody.velocity;
+        stream.Serialize(ref syncVelocity);
     }
+    else
+    {
+        stream.Serialize(ref syncPosition);
+        stream.Serialize(ref syncVelocity);
+ 
+        syncTime = 0f;
+        syncDelay = Time.time - lastSynchronizationTime;
+        lastSynchronizationTime = Time.time;
+ 
+        syncEndPosition = syncPosition + syncVelocity * syncDelay;
+        syncStartPosition = rigidbody.position;
+    }
+}
 }
