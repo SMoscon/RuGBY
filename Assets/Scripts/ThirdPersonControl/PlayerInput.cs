ddd98ec6f6e4d1c18585407110d07efaba071522
SMoscon <Monscou@Monscou.local>
2015-02-20 01:21:59 -0600

    Added Gravity
    lucia can no longer fly

---
+++ Assets/Scripts/ThirdPersonControl/PlayerInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour 
+{
+	private Animator animator;
+
+
+	void Awake()
+	{
+	
+	}
+
+	void Update()
+	{
+	
+	}
+}
