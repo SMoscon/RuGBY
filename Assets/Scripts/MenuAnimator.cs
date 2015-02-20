8df06387351a9995d91d66c88a9dc3cdb3e3dfde
pawsplay <dillon.w.gu@gmail.com>
2015-02-20 04:19:18 -0600

    Animated Camera

---
+++ Assets/Scripts/MenuAnimator.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAnimator : MonoBehaviour {
+	public Animator animator;
+	//public bool Clicked;
+	//public bool Return;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (animator) {
+			/*AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+			if (stateInfo.nameHash == Animator.StringToHash ("Base Layer.CameraAnimation")) {*/
+			//Debug.Log ("clicked: " + CanvasClick.Instance.clicked);
+				//if (CanvasClick.Instance.clicked == true) {
+				//		animator.SetBool ("TurnCamera", true);
+				Debug.Log ("clicked");
+				}
+			//}
+			//else if (stateInfo.nameHash == Animator.StringToHash ("Base Layer.CameraTurn")) {
+				//if (CanvasClick.Instance.returnToMenuz == true) {
+				//		animator.SetBool ("TurnCamera", false);
+				//Debug.Log ("hello");
+				}
+				
+			//}
+		//}
+	
+//	}
+//}
